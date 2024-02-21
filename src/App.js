038ac85a2b52eb79069aacafe4893fc6fab7ec39
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2024-02-21 10:25:49 +0000

    bbc update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-
-
+import f1 from './images/f1.jpg'
+import ben from './images/benstokes.jpg'
+import city from './images/city.jpg'
+import tuchel from './images/tuchel.jpg'
+import './BbcStyles.css'
 const App = () => {
   return (
-    <div>
-      <h1>Info</h1>
-      <Person name="Dave" age="33" petName="Bobby" petType="hamster" />
+    <div className='collection'>
+      <Card img={f1} title="F1 Testing" category="Formula 1"/>
+      <Card img={ben} title="Ben Stokes injured" category="Cricket"/>
+      <Card img={city} title="Haaland scores again" category="Football"/>
+      <Card img={tuchel} title="Tuchel to leave at the end of the season" category="Football"/>
     </div>
   )
 }
 
-const Person = () => {
-  return 
-    <>
-      <h2>name: , age: </h2>
-      <Pet  />
-    </>
-  
-}
-
-const Pet = () => {
+const Card = (props) => {
   return (
-    <div>
-      <p>Has a {} called {}</p>
+    <div className='card'>
+      <img src={props.img} />
+      <h2>{props.title}</h2>
+      <p>{props.category}</p>
     </div>
   )
 }
 export default App
+
+
 
